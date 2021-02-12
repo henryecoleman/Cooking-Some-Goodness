27e60b525045fb235e99d97e8ec6e8236a9edc57
Henry <75659887+henryecoleman@users.noreply.github.com>
2021-02-12 09:58:23 +0000

    Add files via upload

---
+++ RecipeListView.swift
@@ -0,0 +1,50 @@
+//
+//  RecipeListView.swift
+//  Recipe List App
+//
+//  Created by H Coleman on 30/01/2021.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    
+    // Reference the view model
+    @ObservedObject var model = RecipeModel()
+    
+    var body: some View {
+        
+        
+        
+        NavigationView {
+            List(model.recipes) { r in
+                
+                NavigationLink(
+                    destination: RecipeDetailView(recipe:r),
+                    label: {
+                        
+                        // MARK: Row item
+                        HStack(spacing: 20.0) {
+                            Image(r.image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .clipped()
+                                .cornerRadius(5)
+                            Text(r.name)
+                        }
+                    })
+                
+               
+                
+            }
+            .navigationBarTitle("All Recipes")
+        }
+    }
+}
+
+struct RecipeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeListView()
+    }
+}
